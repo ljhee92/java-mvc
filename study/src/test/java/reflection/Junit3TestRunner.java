0ec05981acfebaee66bbc4f11d10109c018183a8
ljhee92 <weilainib7@gmail.com>
2025-09-12 11:21:00 +0900

    feat: test로 시작하는 메서드 실행

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -1,5 +1,7 @@
 package reflection;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 class Junit3TestRunner {
@@ -9,5 +11,17 @@ class Junit3TestRunner {
         Class<Junit3Test> clazz = Junit3Test.class;
 
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        Object testInstance = clazz.getDeclaredConstructor().newInstance();
+
+        Method[] methods = clazz.getMethods();
+        Arrays.stream(methods)
+                .filter(method -> method.getName().startsWith("test"))
+                .forEach(method -> {
+                    try {
+                        method.invoke(testInstance);
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                });
     }
 }
