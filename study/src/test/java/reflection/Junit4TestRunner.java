79d935f117ad1e2d9059688b6ecf26c3765d1199
ljhee92 <weilainib7@gmail.com>
2025-09-12 11:21:14 +0900

    feat: @MyTest 어노테이션이 있는 메서드 실행

--- study/src/test/java/reflection/Junit4TestRunner.java
+++ study/src/test/java/reflection/Junit4TestRunner.java
@@ -1,5 +1,7 @@
 package reflection;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 class Junit4TestRunner {
@@ -9,5 +11,17 @@ class Junit4TestRunner {
         Class<Junit4Test> clazz = Junit4Test.class;
 
         // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        Object testInstance = clazz.getDeclaredConstructor().newInstance();
+
+        Method[] methods = clazz.getMethods();
+        Arrays.stream(methods)
+                .filter(method -> method.isAnnotationPresent(MyTest.class))
+                .forEach(method -> {
+                    try {
+                        method.invoke(testInstance);
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                });
     }
 }
